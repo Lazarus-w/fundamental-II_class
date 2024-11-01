bee225aa094574c236ff608513f27c5d85b84a67
Lazarus_W <your-username@user.noreply.github.com>
2024-11-01 10:45:53 -0300

    this is my third commit

---
+++ strcpy.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+
+const int max=80;
+char str[max]="Devied";
+cout<<str<<endl;
+std::strcpy(str,"theisme");
+cout<<str<<endl;
+
+    return 0;
+}
