b3641281596ca27fa33588bc7a22b52410360021
Lazarus_W <your-username@user.noreply.github.com>
2024-11-10 04:54:36 -0300

    this one is the modified version of the first arry of struct

--- .vscode/arrStuct.cpp
+++ .vscode/arrStuct.cpp
@@ -1,26 +1,24 @@
 #include<iostream>
 using namespace std;
-
 struct student{
-    int rollNUm;
     string name;
+    int rollNUm;
     float grade;
 };
 int main(){
-float sum=0;
+int sum=0;
 for(int i=0;i<5;i++){
-struct student s[5];
-cout<<"enter your rollNUm: ";
-cin>>s[i].rollNUm;
+struct student stu[5];
 cout<<"enter your name: ";
-cin>>s[i].name;
+cin>>stu[i].name;
+cout<<"enter your rollNUm: ";
+cin>>stu[i].rollNUm;
 cout<<"enter your grade: ";
-cin>>s[i].grade;
-sum=+s[i].grade;
+cin>>stu[i].grade;
+sum=+stu[i].grade;
 }
 
-double ave=sum/5;
-cout<<"the average of the five student's is: "<<ave;
+cout<<"the average of the five student's is: "<<sum/5;
 
     return 0;
 }
